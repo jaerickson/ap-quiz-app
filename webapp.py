844020e689600b8efe7e9fafae09a516a6f9b4f5
savaxxahx <savannah@katherynsavannah.com>
2018-01-30 09:22:31 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,22 +1,36 @@
-from flask import Flask, request, Markup, render_template, flash
-import json
 import os
+from flask import Flask, url_for, render_template, request
+from flask import redirect
+from flask import session
 
 app = Flask(__name__)
 
+# In order to use "sessions",you need a "secret key".
+# This is something random you generate.  
+# See: http://flask.pocoo.org/docs/0.10/quickstart/#sessions
+
+app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.  
+                                     #The value should be set in Heroku (Settings->Config Vars). 
+
 def get_questions():
 
-@app.route("/")
+@app.route('/')
 def render_main():
         return render_template('home.html')
     
-@app.route("/lang")
+@app.route('/lang',methods=['GET','POST'])
 def render_lang():
         return render_template('lang.html')
 
-@app.route("/chem")
+@app.route('/chem',methods=['GET','POST'])
 def render_chem():
         return render_template('chem.html')
+
+@app.route('/startOver')
+def startOver():
+    #clear variable values and create a new session
+    session.clear()
+    return redirect(url_for('renderMain')) # url_for('renderMain') could be replaced with '/'
 
 if __name__=="__main__":
     app.run(debug=False)

8a033990dd421a1cbbda432506c32bac3d6e7a1e
kebudke <31251507+kebudke@users.noreply.github.com>
2018-02-13 15:27:38 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -16,8 +16,9 @@ def render_main():
         return render_template('home.html')
     
 @app.route('/lang', methods=['GET','POST'])
+      number = -1
 def render_lang():
-      number = 0
+      number += 1
       with open('values.json') as values_data:
         values = json.load(values_data)
         return render_template('lang.html', q = values[0][number][0][0], val1 = values[0][number][1][0], val2 = values[0][number][2][0], val3 = values[0][number][3][0], val4 = values[0][number][4][0])

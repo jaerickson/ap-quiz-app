a450dd6ccb272f56a59a83713236caeff01c5e31
kebudke <31251507+kebudke@users.noreply.github.com>
2018-02-19 20:32:36 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -14,20 +14,47 @@ app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.
   
   
   
+def get_info(q1, q2, q3, q4, q5, q6):
+    info = "<h4>"
+    if q1 == "correct":
+      info += "Question 1: CORRECT</h4><br>"
+    else:
+      info += "Question 1: INCORRECT</h4><br>"
+    if q2 == "correct":
+      info += "Question 2: CORRECT</h4><br>"
+    else:
+      info += "Question 2: INCORRECT</h4><br>"
+    if q3 == "correct":
+      info += "Question 3: CORRECT</h4><br>"
+    else:
+      info += "Question 3: INCORRECT</h4><br>"
+    if q4 == "correct":
+      info += "Question 4: CORRECT</h4><br>"
+    else:
+      info += "Question 4: INCORRECT</h4><br>"
+    if q5 == "correct":
+      info += "Question 5: CORRECT</h4><br>"
+    else:
+      info += "Question 5: INCORRECT</h4><br>"
+    if q6 == "correct":
+      info += "Question 6: CORRECT</h4><br>"
+    else:
+      info += "Question 6: INCORRECT</h4><br>"
+    return info
+
 @app.route('/')
 def render_main():
         return render_template('home.html')
     
 @app.route('/lang', methods=['GET','POST'])
 def render_lang():
-      with open('values.json') as values_data:
-        values = json.load(values_data)
-        return render_template('lang.html')
-
+  if 'q1' in request.form:
+        return render_template('lang.html', info = get_info(request.form['q1'], request.form['q2'], request.form['q3'], request.form['q4'], request.form['q5'], request.form['q6']))
+  else:
+    return render_template('lang.html')
 @app.route('/chem', methods=['GET','POST'])
 def render_chem():
-      with open('values.json') as values_data:
-        values = json.load(values_data)
+
         return render_template('chem.html')
 
 @app.route('/startOver')
